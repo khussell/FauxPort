1cffa172000260ffd4728740b88ea8db5c40e01f
Lilu <KHussell@gmail.com>
2019-09-20 13:44:49 -0400

    css added to index page

--- index.php
+++ index.php
@@ -12,25 +12,31 @@
 
 <div class="wrapper">
   <div class="nav">
-    <h1>FauxPort</h1>
-    <a href="./index.php">Buy Deals</a>
-    <a href="./create.php">Create Deals</a>
-    <a href="./purchased.php">Purchased Deals</a>
+    <h1 class="logo">FauxPort</h1>
+    <div class="links">
+      <a href="./index.php">Buy Deals</a>
+      <a href="./create.php">Create Deals</a>
+      <a href="./purchased.php">Purchased Deals</a>
+    </div>
+    
   </div>
 
 
 
 
   <div class="todaysDeals">
-    <h1>Today's Deals</h1>
-
+    <img class="transShip" src="./assets/images/transShip.png">
+    <img class="water" width="100%" height="500px" src="./assets/images/water1.png">
+    <div class="dealsContent">
+    <h1 class="todaysDealsTitle">Today's Deals</h1>
+  
 
     <?php
       include("config.php");
     ?>
 
 
-    <div>
+    <div class="cards">
     
     <?php
     $query = $con->query("SELECT * FROM deals WHERE buyerId IS NULL ORDER BY id DESC");
@@ -47,7 +53,7 @@
             $image = $row['image'];
             $id = $row['id'];
 
-            echo "<div class='card'><img height='100px' src='$image'/><h3>$shipName</h3><p>$description</p><p>$$price</p><form action='purchased.php' method='POST'><input type='text' name='name'  placeholder='Your Name'><input style='display:none;' value='$id' name='id'><button type='submit'>Buy!</button></form></div>";
+            echo "<div class='card'><img height='100px' src='$image'/><h3>$shipName</h3><p>$description</p><p>$$price</p><form action='purchased.php' method='POST'><input type='text' name='name'  placeholder='Your Name'><input style='display:none;' value='$id' name='id'><button class='buy' type='submit'>Buy!</button></form></div>";
         }
     } else {
         echo "0 results";
@@ -57,7 +63,7 @@
 
     ?>
 
-
+</div>
 
     </div>
   <div>
