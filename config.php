4cad531bce13d19ade947439369091b62dd08401
Lilu <KHussell@gmail.com>
2019-09-21 18:07:23 -0400

    heroku deploy

--- config.php
+++ config.php
@@ -1,16 +1,27 @@
 <?php
 
-ob_start();
+//ob_start();
 
-try {
-  $con = new PDO("mysql:dbname=fauxport;host=localhost", "root", "flamingo3");
-  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-}
-catch(PDOExeption $e){
-  echo "Connection failed: " . $e->getMessage();
-}
-
-
+//try {
+//  $con = new PDO("mysql:dbname=fauxport;host=localhost", "root", "flamingo3");
+//  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+//}
+//catch(PDOExeption $e){
+//  echo "Connection failed: " . $e->getMessage();
+//}
 
 
+
+
+?>
+
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
 ?>
