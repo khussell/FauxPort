94e5a4d545fe2bf33590df61fa12451241df2e55
Lilu <KHussell@gmail.com>
2019-09-22 12:01:54 -0400

    heroku deploy

--- config.php
+++ config.php
@@ -23,5 +23,16 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$con = new mysqli($server, $username, $password, $db);
+//$con = new mysqli($server, $username, $password, $db);
+
+try {
+  $con = new PDO("mysql:host=$server;dbname=$db", $username, $password);
+  // set the PDO error mode to exception
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+  }
+catch(PDOException $e)
+  {
+  echo "Connection failed: " . $e->getMessage();
+  }
 ?>
